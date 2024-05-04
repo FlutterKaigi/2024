0d05765abf368130e7b92a2b369d237aa50e659a
Tatsuya Okayama (blendthink) <admin@blendthink.dev>
2024-05-05 01:25:27 +0900

    feat: Create the FlutterKaigi 2024 official application

---
+++ apps/app/README.md
@@ -0,0 +1,3 @@
+# conference_2024_app
+
+FlutterKaigi 2024 official application.
---
+++ apps/app/android/app/src/main/kotlin/jp/flutterkaigi/conference_2024_app/MainActivity.kt
@@ -0,0 +1,5 @@
+package jp.flutterkaigi.conference_2024_app
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
