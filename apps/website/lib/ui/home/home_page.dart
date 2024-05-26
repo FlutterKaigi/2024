56521d47b2c6a16e1c896bac0ca63570ddacae5e
YumNumm <onory0917@gmail.com>
2024-05-26 17:10:27 +0900

    add: background

--- apps/website/lib/ui/home/home_page.dart
+++ apps/website/lib/ui/home/home_page.dart
@@ -1,29 +1,59 @@
+import 'package:conference_2024_website/ui/home/components/background/background_bottom.dart';
+import 'package:conference_2024_website/ui/home/components/background/background_top.dart';
 import 'package:conference_2024_website/ui/theme/extension/theme_extension.dart';
 import 'package:flutter/material.dart';
-import 'package:hooks_riverpod/hooks_riverpod.dart';
 
-final class HomePage extends HookConsumerWidget {
+final class HomePage extends StatelessWidget {
   const HomePage({super.key});
 
   @override
-  Widget build(BuildContext context, WidgetRef ref) {
-    final theme = Theme.of(context);
-    final textTheme = theme.customThemeExtension.textTheme;
-    return Scaffold(
-      appBar: AppBar(
-        title: const Text('Home'),
-      ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Text(
-              'FlutterKaigi 2024',
-              style: textTheme.headline,
-            ),
-          ],
+  Widget build(BuildContext context) {
+    return const Scaffold(
+      body: SingleChildScrollView(
+        child: IntrinsicHeight(
+          child: Stack(
+            children: [
+              _Background(),
+              Padding(
+                padding: EdgeInsets.all(16),
+                child: Column(
+                  children: [
+                    _Body(),
+                  ],
+                ),
+              ),
+            ],
+          ),
         ),
       ),
+    );
+  }
+}
+
+class _Body extends StatelessWidget {
+  const _Body();
+
+  @override
+  Widget build(BuildContext context) {
+    final theme = Theme.of(context);
+    final textTheme = theme.customThemeExtension.textTheme;
+    return Text(
+      'Hello, World!',
+      style: textTheme.headline,
+    );
+  }
+}
+
+class _Background extends StatelessWidget {
+  const _Background();
+
+  @override
+  Widget build(BuildContext context) {
+    return const Column(
+      children: [
+        BackgroundTop(),
+        BackgroundBottom(),
+      ],
     );
   }
 }
