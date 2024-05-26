fc127f918f7df9d07589280fcd4c3b66760ea024
YumNumm <onory0917@gmail.com>
2024-05-26 15:40:23 +0900

    fix: main.dart

--- apps/website/lib/main.dart
+++ apps/website/lib/main.dart
@@ -1,6 +1,5 @@
 import 'package:conference_2024_website/ui/router/router.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter_gen/gen_l10n/l10n.dart';
 
 void main() {runApp(const MainApp());
 }
@@ -11,8 +10,6 @@ class MainApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp.router(
-      localizationsDelegates: L10n.localizationsDelegates,
-      supportedLocales: L10n.supportedLocales,
       locale: const Locale('ja'),
       routerDelegate: router.routerDelegate,
       routeInformationParser: router.routeInformationParser,
