ec2990898c75fe23875cc451595ca2442d838c8b
YumNumm <onory0917@gmail.com>
2024-05-30 04:22:06 +0900

    fix: title部分のscale

--- apps/website/lib/ui/home/components/title_and_logo.dart
+++ apps/website/lib/ui/home/components/title_and_logo.dart
@@ -10,60 +10,67 @@ class TitleAndLogo extends StatelessWidget {
     final textTheme = theme.customThemeExtension.textTheme;
     final colorTheme = theme.customThemeExtension.colorTheme;
 
-    return FittedBox(
-      fit: BoxFit.scaleDown,
-      child: Column(
-        mainAxisSize: MainAxisSize.min,
-        crossAxisAlignment: CrossAxisAlignment.end,
-        children: [
-          Row(
-            mainAxisSize: MainAxisSize.min,
+    return Column(
+      mainAxisSize: MainAxisSize.min,
+      crossAxisAlignment: CrossAxisAlignment.end,
+      children: [
+        FittedBox(
+          fit: BoxFit.scaleDown,
+          child: Column(
             children: [
-              Image.asset(
-                'assets/images/icon.png',
-                height: 94,
-                width: 94,
+              Row(
+                mainAxisSize: MainAxisSize.min,
+                children: [
+                  Image.asset(
+                    'assets/images/icon.png',
+                    height: 94,
+                    width: 94,
+                  ),
+                  const SizedBox(width: 12),
+                  Text(
+                    'FlutterKaigi',
+                    style: textTheme.availableFonts.poppins.medium.copyWith(
+                      fontSize: 87,
+                    ),
+                  ),
+                ],
               ),
-              const SizedBox(width: 12),
-              Text(
-                'FlutterKaigi',
-                style: textTheme.availableFonts.poppins.medium.copyWith(
-                  fontSize: 87,
-                ),
+              Row(
+                mainAxisSize: MainAxisSize.min,
+                crossAxisAlignment: CrossAxisAlignment.end,
+                children: [
+                  Text(
+                    'November\n21(Thu) - 22(Fri)',
+                    style: textTheme.availableFonts.poppins.regular.copyWith(
+                      fontSize: 33,
+                      color: colorTheme.grey,
+                    ),
+                    textAlign: TextAlign.end,
+                  ),
+                  const SizedBox(width: 40),
+                  Text(
+                    '2024',
+                    style: textTheme.availableFonts.poppins.bold.copyWith(
+                      fontSize: 120,
+                      // Baseline上下の余白を消すため
+                      // 2024を表示する時にその余白は不要
+                      height: 0.8,
+                    ),
+                  ),
+                ],
               ),
+              const SizedBox(height: 12),
             ],
           ),
-          Row(
-            mainAxisSize: MainAxisSize.min,
-            crossAxisAlignment: CrossAxisAlignment.end,
-            children: [
-              Text(
-                'November\n21(Thu) - 22(Fri)',
-                style: textTheme.availableFonts.poppins.regular.copyWith(
-                  fontSize: 33,
-                  color: colorTheme.grey,
-                ),
-                textAlign: TextAlign.end,
-              ),
-              const SizedBox(width: 40),
-              Text(
-                '2024',
-                style: textTheme.availableFonts.poppins.bold.copyWith(
-                  fontSize: 120,
-                  // Baseline上下の余白を消すため
-                  // 2024を表示する時にその余白は不要
-                  height: 0.8,
-                ),
-              ),
-            ],
-          ),
-          const SizedBox(height: 12),
-          Text(
+        ),
+        FittedBox(
+          fit: BoxFit.scaleDown,
+          child: Text(
             '有明セントラルタワーホール&カンファレンス',
             style: textTheme.body,
           ),
-        ],
-      ),
+        ),
+      ],
     );
   }
 }
