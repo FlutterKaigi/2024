126d91539a11317cbc7245c5fdb851f79be859aa
YumNumm <onory0917@gmail.com>
2024-06-02 04:36:31 +0900

    fix: import path

--- apps/website/lib/ui/home/home_page.dart
+++ apps/website/lib/ui/home/home_page.dart
@@ -1,10 +1,10 @@
+import 'package:conference_2024_website/l10n/l10n.dart';
 import 'package:conference_2024_website/ui/components/button/app_button.dart';
 import 'package:conference_2024_website/ui/home/components/background/background_bottom.dart';
 import 'package:conference_2024_website/ui/home/components/background/background_top.dart';
 import 'package:conference_2024_website/ui/home/components/title_and_logo.dart';
 import 'package:conference_2024_website/ui/theme/extension/theme_extension.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter_gen/gen_l10n/app_localizations.dart';
 
 final class HomePage extends StatelessWidget {
   const HomePage({super.key});
