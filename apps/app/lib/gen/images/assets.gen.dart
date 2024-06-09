427ab22c5af4ce2d42283d55e4d36ef4a88a472e
Tatsuya Okayama (blendthink) <admin@blendthink.dev>
2024-06-09 21:17:09 +0900

    feat: app に flutter_gen を導入

---
+++ apps/app/lib/gen/images/assets.gen.dart
@@ -0,0 +1,12 @@
+/// GENERATED CODE - DO NOT MODIFY BY HAND
+/// *****************************************************
+///  FlutterGen
+/// *****************************************************
+
+// coverage:ignore-file
+// ignore_for_file: type=lint
+// ignore_for_file: directives_ordering,unnecessary_import,implicit_dynamic_list_literal,deprecated_member_use
+
+class Assets {
+  Assets._();
+}
