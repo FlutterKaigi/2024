167ecedc0626ce39636bc40f1a46c56bc1fa41fe
Tatsuya Okayama (blendthink) <admin@blendthink.dev>
2024-06-09 16:53:24 +0900

    feat: Add riverpod to app

--- apps/app/lib/main.dart
+++ apps/app/lib/main.dart
@@ -1,7 +1,12 @@
 import 'package:flutter/material.dart';
+import 'package:hooks_riverpod/hooks_riverpod.dart';
 
 void main() {
-  runApp(const MainApp());
+  runApp(
+    const ProviderScope(
+      child: MainApp(),
+    ),
+  );
 }
 
 class MainApp extends StatelessWidget {
