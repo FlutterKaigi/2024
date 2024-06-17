639f37b75b5189bf57c2e954d4b49cfd5d0c9f44
Tatsuya Okayama (blendthink) <admin@blendthink.dev>
2024-06-18 00:36:27 +0900

    feat: デバッグページから任意のページに遷移できるように実装

--- packages/app/features/debug/lib/src/ui/debug_page.dart
+++ packages/app/features/debug/lib/src/ui/debug_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:go_router/go_router.dart';
 
 class DebugPage extends StatelessWidget {
   const DebugPage({
@@ -7,14 +8,61 @@ class DebugPage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
+    final router = GoRouter.of(context);
+    final goRoutes = router.configuration.routes.toGoRoutes();
+    return Scaffold(
       body: CustomScrollView(
         slivers: [
-          SliverAppBar.large(
+          const SliverAppBar.large(
             title: Text('Debug Page'),
+          ),
+          SliverToBoxAdapter(
+            child: DropdownMenu<GoRoute>(
+              dropdownMenuEntries: goRoutes
+                  .map(
+                    (goRoute) {
+                      // debug routes are excluded
+                      if (goRoute.path.contains('debug')) {
+                        return null;
+                      }
+                      return DropdownMenuEntry<GoRoute>(
+                        value: goRoute,
+                        label: goRoute.path,
+                      );
+                    },
+                  )
+                  .nonNulls
+                  .toList(),
+              onSelected: (goRoute) {
+                if (goRoute == null) {
+                  return;
+                }
+                router.go(goRoute.path);
+              },
+            ),
           ),
         ],
       ),
     );
+  }
+}
+
+extension _ToGoRoutes on List<RouteBase> {
+  List<GoRoute> toGoRoutes() {
+    final goRoutes = <GoRoute>[];
+    for (final route in this) {
+      switch (route) {
+        case GoRoute():
+          final childRoutes = route.routes;
+          if (childRoutes.isEmpty) {
+            goRoutes.add(route);
+          } else {
+            goRoutes.addAll(childRoutes.toGoRoutes());
+          }
+        case ShellRoute() || StatefulShellRoute():
+          goRoutes.addAll(route.routes.toGoRoutes());
+      }
+    }
+    return goRoutes;
   }
 }
