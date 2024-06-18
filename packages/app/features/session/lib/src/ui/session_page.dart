03d9fb523c98baeecb5e34491019e7db98319e3f
Tatsuya Okayama (blendthink) <admin@blendthink.dev>
2024-06-18 23:56:26 +0900

    feat: セッション詳細ページで sessionId を受け取る

--- packages/app/features/session/lib/src/ui/session_page.dart
+++ packages/app/features/session/lib/src/ui/session_page.dart
@@ -3,14 +3,17 @@ import 'package:packages_app_features_session/l10n.dart';
 
 class SessionPage extends StatelessWidget {
   const SessionPage({
+    required String sessionId,
     super.key,
-  });
+  }) : _sessionId = sessionId;
+
+  final String _sessionId;
 
   @override
   Widget build(BuildContext context) {
     final l = L10nSession.of(context);
     return Center(
-      child: Text(l.sessionPageTitle),
+      child: Text('${l.sessionPageTitle}: $_sessionId'),
     );
   }
 }
