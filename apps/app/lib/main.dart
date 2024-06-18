a66d524c254209df310c2f1983099cbe54b38dc6
Tatsuya Okayama (blendthink) <admin@blendthink.dev>
2024-06-19 01:02:16 +0900

    feat: デバッグページの多言語対応

--- apps/app/lib/main.dart
+++ apps/app/lib/main.dart
@@ -6,6 +6,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
 import 'package:packages_app_features_about/l10n.dart';
+import 'package:packages_app_features_debug/l10n.dart';
 import 'package:packages_app_features_news/l10n.dart';
 import 'package:packages_app_features_session/l10n.dart';
 import 'package:packages_app_features_venue/l10n.dart';
@@ -32,6 +33,7 @@ class MainApp extends ConsumerWidget {
         ...L10nNews.localizationsDelegates,
         ...L10nSession.localizationsDelegates,
         ...L10nVenue.localizationsDelegates,
+        ...L10nDebug.localizationsDelegates,
       ],
       supportedLocales: const [
         ...L10n.supportedLocales,
@@ -39,6 +41,7 @@ class MainApp extends ConsumerWidget {
         ...L10nNews.supportedLocales,
         ...L10nSession.supportedLocales,
         ...L10nVenue.supportedLocales,
+        ...L10nDebug.supportedLocales,
       ],
       shortcuts: {
         LogicalKeySet(
---
+++ packages/app/features/debug/lib/src/gen/l10n/l10n_ja.dart
@@ -0,0 +1,11 @@
+// ignore_for_file: type=lint
+
+import 'l10n.dart';
+
+/// The translations for Japanese (`ja`).
+class L10nDebugJa extends L10nDebug {
+  L10nDebugJa([String locale = 'ja']) : super(locale);
+
+  @override
+  String get routeDropDownHintText => 'Select a route';
+}
