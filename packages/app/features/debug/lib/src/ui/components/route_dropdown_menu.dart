b475713b0fedd2701c210127628a867493b0bf6d
Tatsuya Okayama (blendthink) <admin@blendthink.dev>
2024-06-18 23:02:42 +0900

    feat: dropdownMenuEntries をメモ化

--- packages/app/features/debug/lib/src/ui/components/route_dropdown_menu.dart
+++ packages/app/features/debug/lib/src/ui/components/route_dropdown_menu.dart
@@ -1,28 +1,35 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_hooks/flutter_hooks.dart';
 import 'package:go_router/go_router.dart';
 
-final class RouteDropdownMenu extends StatelessWidget {
+final class RouteDropdownMenu extends HookWidget {
   const RouteDropdownMenu({super.key});
 
   @override
   Widget build(BuildContext context) {
     final router = GoRouter.of(context);
-    final goRoutes = router.configuration.routes.toGoRoutes();
-    final dropdownMenuEntries = goRoutes
-        .map(
-          (goRoute) {
-            // debug routes are excluded
-            if (goRoute.path.contains('debug')) {
-              return null;
-            }
-            return DropdownMenuEntry<GoRoute>(
-              value: goRoute,
-              label: goRoute.path,
-            );
-          },
-        )
-        .nonNulls
-        .toList();
+    final routeBases = router.configuration.routes;
+    final dropdownMenuEntries = useMemoized(
+      () {
+        final goRoutes = routeBases.toGoRoutes();
+        return goRoutes
+            .map(
+              (goRoute) {
+                // debug routes are excluded
+                if (goRoute.path.contains('debug')) {
+                  return null;
+                }
+                return DropdownMenuEntry<GoRoute>(
+                  value: goRoute,
+                  label: goRoute.path,
+                );
+              },
+            )
+            .nonNulls
+            .toList();
+      },
+      routeBases,
+    );
     return LayoutBuilder(
       builder: (context, constraints) {
         return DropdownMenu<GoRoute>(
