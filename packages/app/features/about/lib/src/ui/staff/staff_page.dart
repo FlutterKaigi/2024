3301c60618edde7a3c4cfd28bc2ff70595cc081d
TK-Guitar <tk39business@gmail.com>
2024-06-23 21:21:39 +0900

    ListViewに変更しSafeAreaを考慮しました

--- packages/app/features/about/lib/src/ui/staff/staff_page.dart
+++ packages/app/features/about/lib/src/ui/staff/staff_page.dart
@@ -20,33 +20,32 @@ class StaffPage extends StatelessWidget {
           icon: const Icon(Icons.arrow_back),
         ),
       ),
-      body: SingleChildScrollView(
-        child: Padding(
-          padding: const EdgeInsets.only(right: 16, left: 16),
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              const SizedBox(
-                height: 40,
-              ),
-              Text(
-                _name,
-                style: const TextStyle(fontSize: 32),
-              ),
-              const SizedBox(
-                height: 28,
-              ),
-              // TODO:モックデータとしてWidgetを出してます。
-              // https://github.com/FlutterKaigi/2024/issues/122
-              for (int i = 0; i < 50; i++)
-                StaffCardWidget(
-                  name: 'Staff${++i} さん',
-                  imageUrl:
-                      'https://pbs.twimg.com/profile_images/1797556194556710912/ZncGhPyV_400x400.png',
-                ),
-            ],
-          ),
+      body: ListView(
+        padding: EdgeInsets.only(
+          bottom: MediaQuery.paddingOf(context).bottom,
+          right: 16,
+          left: 16,
         ),
+        children: [
+          const SizedBox(
+            height: 40,
+          ),
+          Text(
+            _name,
+            style: const TextStyle(fontSize: 32),
+          ),
+          const SizedBox(
+            height: 28,
+          ),
+          // TODO:モックデータとしてWidgetを出してます。
+          // https://github.com/FlutterKaigi/2024/issues/122
+          for (int i = 0; i < 50; i++)
+            StaffCardWidget(
+              name: 'Staff${++i} さん',
+              imageUrl:
+                  'https://pbs.twimg.com/profile_images/1797556194556710912/ZncGhPyV_400x400.png',
+            ),
+        ],
       ),
     );
   }
