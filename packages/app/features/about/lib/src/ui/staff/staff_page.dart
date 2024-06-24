e178deb703e667c2f040ffcad3bcad3012d78ee5
TK-Guitar <tk39business@gmail.com>
2024-06-24 20:25:04 +0900

    指摘対応しました

--- packages/app/features/about/lib/src/ui/staff/staff_page.dart
+++ packages/app/features/about/lib/src/ui/staff/staff_page.dart
@@ -11,41 +11,32 @@ class StaffPage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    final theme = Theme.of(context);
-
     return Scaffold(
-      appBar: AppBar(
-        leading: IconButton(
-          onPressed: () {
-            Navigator.of(context).pop();
-          },
-          icon: const Icon(Icons.arrow_back),
-        ),
-      ),
-      body: ListView(
-        padding: EdgeInsets.only(
-          bottom: MediaQuery.paddingOf(context).bottom,
-          left: 16,
-        ),
-        children: [
-          const SizedBox(
-            height: 40,
+      body: CustomScrollView(
+        slivers: [
+          SliverAppBar.large(
+            title: Text(
+              _name,
+            ),
           ),
-          Text(
-            _name,
-            style: theme.textTheme.titleLarge!.copyWith(fontSize: 32),
-          ),
-          const SizedBox(
-            height: 28,
-          ),
+
           // TODO:モックデータとしてWidgetを出してます。
           // https://github.com/FlutterKaigi/2024/issues/122
-          for (int i = 0; i < 50; i++)
-            StaffCardWidget(
-              name: 'Staff${++i} さん',
-              imageUrl:
-                  'https://pbs.twimg.com/profile_images/1797556194556710912/ZncGhPyV_400x400.png',
+          SliverPadding(
+            padding: const EdgeInsets.only(left: 16),
+            sliver: SliverFixedExtentList(
+              itemExtent: 76,
+              delegate: SliverChildBuilderDelegate(
+                (context, index) {
+                  return const StaffCardWidget(
+                    name: 'Staffさん',
+                    imageUrl:
+                        'https://pbs.twimg.com/profile_images/1797556194556710912/ZncGhPyV_400x400.png',
+                  );
+                },
+              ),
             ),
+          ),
         ],
       ),
     );
