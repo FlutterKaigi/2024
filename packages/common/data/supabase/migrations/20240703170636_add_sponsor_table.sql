cdf8dbd3d373dcb0389f02ccc17dd51f0acb3318
YumNumm <onory0917@gmail.com>
2024-07-06 18:04:30 +0900

    chore: rename sponsor table -> sponsors

--- packages/common/data/supabase/migrations/20240703170636_add_sponsor_table.sql
+++ packages/common/data/supabase/migrations/20240703170636_add_sponsor_table.sql
@@ -1,18 +1,18 @@
 CREATE TYPE sponsor_type AS ENUM ('platinum', 'gold', 'silver', 'bronze');
 
 CREATE TABLE
-  IF NOT EXISTS sponsor (
+  IF NOT EXISTS sponsors (
     id SERIAL PRIMARY KEY NOT NULL,
     name TEXT NOT NULL,
     logo_name TEXT NOT NULL,
-    /* https://[project_id].supabase.co/storage/v1/object/public/sponsor/[logo_name] でアクセス可能 */
+    /* https://[project_id].supabase.co/storage/v1/object/public/sponsors/[logo_name] でアクセス可能 */
     description TEXT NOT NULL,
     url TEXT NULL,
     type sponsor_type NOT NULL
   );
 
-ALTER TABLE sponsor ENABLE ROW LEVEL SECURITY;
+ALTER TABLE sponsors ENABLE ROW LEVEL SECURITY;
 
-CREATE POLICY "Everyone can read sponsor" ON sponsor
+CREATE POLICY "Everyone can read sponsor" ON sponsors
 FOR SELECT
   USING (true);
