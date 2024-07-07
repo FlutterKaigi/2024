0caa41fe255da30cd3c75a8611cca61b995f96ec
YumNumm <73390859+YumNumm@users.noreply.github.com>
2024-07-07 07:45:47 +0000

    Auto format for SQL files

--- packages/common/data/supabase/migrations/20240703170636_add_sponsor_table.sql
+++ packages/common/data/supabase/migrations/20240703170636_add_sponsor_table.sql
@@ -1,18 +1,17 @@
-CREATE TYPE sponsor_type AS ENUM ('platinum', 'gold', 'silver', 'bronze');
+CREATE TYPE sponsor_type AS enum('platinum', 'gold', 'silver', 'bronze');
 
-CREATE TABLE
-  IF NOT EXISTS sponsors (
-    id SERIAL PRIMARY KEY NOT NULL,
-    name TEXT NOT NULL,
-    logo_name TEXT NOT NULL,
-    /* https://[project_id].supabase.co/storage/v1/object/public/sponsors/[logo_name] でアクセス可能 */
-    description TEXT NOT NULL,
-    url TEXT NULL,
-    type sponsor_type NOT NULL
-  );
+CREATE TABLE IF NOT EXISTS sponsors (
+  id serial PRIMARY KEY NOT NULL,
+  name text NOT NULL,
+  logo_name text NOT NULL,
+  /* https://[project_id].supabase.co/storage/v1/object/public/sponsors/[logo_name] でアクセス可能 */
+  description text NOT NULL,
+  url text NULL,
+  type sponsor_type NOT NULL
+);
 
-ALTER TABLE sponsors ENABLE ROW LEVEL SECURITY;
+ALTER TABLE sponsors enable ROW level security;
 
-CREATE POLICY "Everyone can read sponsor" ON sponsors
-FOR SELECT
-  USING (true);
+CREATE POLICY "Everyone can read sponsor" ON sponsors FOR
+SELECT
+  USING (TRUE);
