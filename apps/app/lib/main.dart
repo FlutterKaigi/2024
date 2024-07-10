c983a89cb62b8e4ff52599bc93613d156589a118
Tatsuya Okayama (blendthink) <admin@blendthink.dev>
2024-07-10 22:46:12 +0900

    feat: DynamicColor, Theme をアプリに適用

--- apps/app/lib/main.dart
+++ apps/app/lib/main.dart
@@ -1,7 +1,9 @@
 import 'dart:async';
 
+import 'package:app_cores_designsystem/theme.dart';
 import 'package:conference_2024_app/gen/l10n/l10n.dart';
 import 'package:conference_2024_app/routing/router.dart';
+import 'package:dynamic_color/dynamic_color.dart';
 import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
@@ -27,41 +29,50 @@ class MainApp extends ConsumerWidget {
   @override
   Widget build(BuildContext context, WidgetRef ref) {
     final router = ref.watch(routerProvider);
-    return MaterialApp.router(
-      routerConfig: router,
-      localizationsDelegates: const [
-        ...L10n.localizationsDelegates,
-        ...L10nAbout.localizationsDelegates,
-        ...L10nNews.localizationsDelegates,
-        ...L10nSession.localizationsDelegates,
-        ...L10nVenue.localizationsDelegates,
-        ...L10nDebug.localizationsDelegates,
-      ],
-      supportedLocales: const [
-        ...L10n.supportedLocales,
-        ...L10nAbout.supportedLocales,
-        ...L10nNews.supportedLocales,
-        ...L10nSession.supportedLocales,
-        ...L10nVenue.supportedLocales,
-        ...L10nDebug.supportedLocales,
-      ],
-      shortcuts: kDebugMode
-          ? {
-              LogicalKeySet(
-                LogicalKeyboardKey.shift,
-                LogicalKeyboardKey.keyD,
-              ): const _DebugIntent(),
-            }
-          : null,
-      actions: kDebugMode
-          ? <Type, Action<Intent>>{
-              _DebugIntent: CallbackAction<_DebugIntent>(
-                onInvoke: (_) => unawaited(
-                  router.push(const DebugPageRoute().location),
-                ),
-              ),
-            }
-          : null,
+    return DynamicColorBuilder(
+      builder: (lightDynamic, darkDynamic) {
+        final theme = ref.watch(themeProvider(lightDynamic));
+        final darkTheme = ref.watch(darkThemeProvider(darkDynamic));
+
+        return MaterialApp.router(
+          theme: theme,
+          darkTheme: darkTheme,
+          routerConfig: router,
+          localizationsDelegates: const [
+            ...L10n.localizationsDelegates,
+            ...L10nAbout.localizationsDelegates,
+            ...L10nNews.localizationsDelegates,
+            ...L10nSession.localizationsDelegates,
+            ...L10nVenue.localizationsDelegates,
+            ...L10nDebug.localizationsDelegates,
+          ],
+          supportedLocales: const [
+            ...L10n.supportedLocales,
+            ...L10nAbout.supportedLocales,
+            ...L10nNews.supportedLocales,
+            ...L10nSession.supportedLocales,
+            ...L10nVenue.supportedLocales,
+            ...L10nDebug.supportedLocales,
+          ],
+          shortcuts: kDebugMode
+              ? {
+                  LogicalKeySet(
+                    LogicalKeyboardKey.shift,
+                    LogicalKeyboardKey.keyD,
+                  ): const _DebugIntent(),
+                }
+              : null,
+          actions: kDebugMode
+              ? <Type, Action<Intent>>{
+                  _DebugIntent: CallbackAction<_DebugIntent>(
+                    onInvoke: (_) => unawaited(
+                      router.push(const DebugPageRoute().location),
+                    ),
+                  ),
+                }
+              : null,
+        );
+      },
     );
   }
 }
