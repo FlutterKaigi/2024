21a5b86f580c425c2d7d4403b0d300fbe05fc1bf
Tatsuya Okayama (blendthink) <admin@blendthink.dev>
2024-07-15 05:57:08 +0900

    fix: greeting をデフォルトから文字に設定

--- packages/common/data/supabase/migrations/20240713092216_add_staffs_table.sql
+++ packages/common/data/supabase/migrations/20240713092216_add_staffs_table.sql
@@ -2,7 +2,7 @@ CREATE TABLE IF NOT EXISTS staffs (
   id smallserial PRIMARY KEY NOT NULL,
   name text NOT NULL,
   icon_name text NOT NULL, -- https://[project_id].supabase.co/storage/v1/object/public/staffs/[icon_name]
-  greeting text
+  greeting text DEFAULT '' NOT NULL
 );
 
 ALTER TABLE staffs enable ROW level security;
