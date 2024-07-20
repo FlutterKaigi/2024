014f4a00f43e4741b0c3cda44734730aee02d533
YumNumm <onory0917@gmail.com>
2024-07-20 18:23:00 +0900

    add: unit tests

---
+++ packages/common/data/supabase/tests/staffs_test.sql
@@ -0,0 +1,65 @@
+BEGIN;
+
+SELECT
+  dbdev.install ('basejump-supabase_test_helpers');
+
+CREATE EXTENSION "basejump-supabase_test_helpers";
+
+-- 事前準備: スタッフを追加
+DELETE FROM staffs;
+
+INSERT INTO
+  staffs (name, icon_name, greeting)
+VALUES
+  ('Sample Staff', 'sample_staff_icon.png', 'This is a sample staff');
+
+-- 事前準備: テスト用ユーザを作成
+SELECT
+  tests.create_supabase_user ('sample_user', 'example@example.com', 'password');
+
+SELECT
+  tests.authenticate_as ('sample_user');
+
+SELECT
+  plan (6);
+
+SELECT
+  has_table ('public', 'staffs', 'staffs テーブルが存在すること');
+
+-- staffs テーブルの RLS が有効になっていること
+SELECT
+  tests.rls_enabled ('public', 'staffs');
+
+SELECT
+  results_eq ('SELECT name FROM staffs', ARRAY['Sample Staff'], '任意のユーザはサンプルスタッフを読み取れること');
+
+PREPARE insert_throw AS
+INSERT INTO
+  staffs (name, icon_name, greeting)
+VALUES
+  ('another sample staff', 'another_sample_staff_icon.png', 'this is another sample staff');
+
+SELECT
+  throws_ok ('insert_throw', '42501', 'new row violates row-level security policy for table "staffs"', '任意のユーザはスタッフを追加できないこと');
+
+-- スタッフを削除できないこと
+DELETE FROM staffs;
+
+SELECT
+  results_eq ('SELECT COUNT(*) FROM staffs', ARRAY[1::bigint], 'スタッフを削除できていないこと');
+
+-- スタッフを更新できないこと
+PREPARE update_throw AS
+UPDATE staffs
+SET
+  name = 'updated sample staff';
+
+SELECT
+  results_eq ('SELECT name FROM staffs', ARRAY['Sample Staff'], 'スタッフを更新できていないこと');
+
+SELECT
+  *
+FROM
+  finish ();
+
+ROLLBACK;
