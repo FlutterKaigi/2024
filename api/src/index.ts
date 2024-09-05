c97e48c81656f65f3325e8835283bccd713ddc6e
YumNumm <onory0917@gmail.com>
2024-09-06 01:01:05 +0900

    initialize api directory

---
+++ api/src/index.ts
@@ -0,0 +1,26 @@
+import { logger } from "hono/logger";
+import { csrf } from "hono/csrf";
+import { cors } from "hono/cors";
+import { secureHeaders } from "hono/secure-headers";
+import { prettyJSON } from "hono/pretty-json";
+import { Hono } from "hono";
+import { Bindings } from "./binding";
+
+import coupon from "./routes/coupon";
+
+const app = new Hono<{ Bindings: Bindings }>();
+
+app.use(
+  "*",
+  cors({
+    origin: "flutterkaigi-ticket-2024.pages.dev"
+  })
+);
+app.use(logger());
+app.use(csrf());
+app.use(secureHeaders());
+app.use(prettyJSON());
+
+app.route("/coupon", coupon);
+
+export default app;
