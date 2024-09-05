ed99f7d2f833215eb9f8804008e3bfcc78c17c59
YumNumm <onory0917@gmail.com>
2024-09-06 02:18:58 +0900

    fix: テストエンドポイントの作成

--- api/src/index.ts
+++ api/src/index.ts
@@ -1,5 +1,4 @@
 import { logger } from "hono/logger";
-import { csrf } from "hono/csrf";
 import { cors } from "hono/cors";
 import { secureHeaders } from "hono/secure-headers";
 import { prettyJSON } from "hono/pretty-json";
@@ -7,6 +6,9 @@ import { Hono } from "hono";
 import { Bindings } from "./bindings";
 
 import coupon from "./routes/coupon";
+import { getUserWithProfile } from "./util/user";
+import { createClient } from "@supabase/supabase-js";
+import { Database } from "./util/supabaseSchema";
 
 const app = new Hono<{ Bindings: Bindings }>();
 
@@ -17,10 +19,25 @@ app.use(
   })
 );
 app.use(logger());
-app.use(csrf());
 app.use(secureHeaders());
 app.use(prettyJSON());
 
-app.route("/coupon", coupon);
+// MEMO(YumNumm): 動作検証用のテストエンドポイント
+// TODO(YumNumm): 削除する
+app.get("/auth", async (c) => {
+  const authorization = c.req.header("Authorization");
+  if (!authorization) {
+    return c.text("Unauthorized", 401);
+  }
+
+  const supabase = createClient<Database>(
+    c.env.SUPABASE_URL,
+    c.env.SUPABASE_KEY
+  );
+
+  const result = await getUserWithProfile(authorization, supabase);
+
+  return c.json(result);
+});
 
 export default app;
