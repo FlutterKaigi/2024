0d664e7d1aeb0cb843c4ade6bfe9604bb97c9c4c
YumNumm <onory0917@gmail.com>
2024-09-06 03:19:42 +0900

    add: App classとRouterの紐付け

--- apps/ticket_web/lib/app.dart
+++ apps/ticket_web/lib/app.dart
@@ -1,10 +1,17 @@
 import 'package:flutter/material.dart';
+import 'package:hooks_riverpod/hooks_riverpod.dart';
+import 'package:ticket_web/core/router/router.dart';
 
-class App extends StatelessWidget {
+class App extends ConsumerWidget {
   const App({super.key});
 
   @override
-  Widget build(BuildContext context) {
-    return const Placeholder();
+  Widget build(BuildContext context, WidgetRef ref) {
+    final router = ref.watch(routerProvider);
+
+    return MaterialApp.router(
+      title: 'Ticket System',
+      routerConfig: router,
+    );
   }
 }
