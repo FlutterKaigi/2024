8467fa7813c77487d8a3dd71c7e1229c27c7409a
TK-Guitar <tk39business@gmail.com>
2024-09-09 17:51:15 +0900

    セッションページのカードWidgetを作成しました

---
+++ packages/app/features/session/lib/src/ui/session_item.dart
@@ -0,0 +1,105 @@
+import 'package:app_cores_designsystem/ui.dart';
+import 'package:flutter/material.dart';
+import 'package:gap/gap.dart';
+
+/// タイムラインのアイテム
+class SessionItem extends StatelessWidget {
+  const SessionItem({
+    required String name,
+    super.key,
+  }) : _name = name;
+
+  final String _name;
+
+  @override
+  Widget build(BuildContext context) {
+    final theme = Theme.of(context);
+    return Card(
+      elevation: 0,
+      shape: RoundedRectangleBorder(
+        borderRadius: BorderRadius.circular(12),
+        side: BorderSide(
+          color: Theme.of(context).colorScheme.outline,
+        ),
+      ),
+      clipBehavior: Clip.antiAlias,
+      child: ListTile(
+        contentPadding: const EdgeInsets.symmetric(
+          horizontal: 16,
+          vertical: 16,
+        ),
+        title: Text(
+          _name,
+          style: theme.textTheme.titleMedium,
+        ),
+        subtitle: Column(
+          children: [
+            const Gap(8),
+            Row(
+              children: [
+                ClipRRect(
+                  borderRadius: BorderRadius.circular(12),
+                  child: Image.network(
+                    height: 40,
+                    width: 40,
+                    'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQalMJeasBiucOyCl7CwofX0mtDRL77WPt6XA&s',
+                    errorBuilder: (context, error, stackTrace) {
+                      return const MainLogo();
+                    },
+                  ),
+                ),
+                const Gap(8),
+                Text(
+                  'Test2',
+                  style: theme.textTheme.labelMedium,
+                ),
+              ],
+            ),
+            const Gap(8),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                Row(
+                  children: [
+                    Container(
+                      padding: const EdgeInsets.symmetric(horizontal: 12),
+                      height: 28,
+                      decoration: BoxDecoration(
+                        color: const Color(0xff81d3dd),
+                        borderRadius: BorderRadius.circular(25),
+                      ),
+                      child: Center(
+                        child: Text(
+                          'DevOps',
+                          style: theme.textTheme.labelSmall,
+                        ),
+                      ),
+                    ),
+                    const Gap(8),
+                    Container(
+                      padding: const EdgeInsets.symmetric(horizontal: 12),
+                      height: 28,
+                      decoration: BoxDecoration(
+                        color: const Color(0xffB8C6EA),
+                        borderRadius: BorderRadius.circular(25),
+                      ),
+                      child: Center(
+                        child: Text(
+                          'Room1',
+                          style: theme.textTheme.labelSmall,
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
+                const Icon(
+                  Icons.bookmark_outline,
+                ),
+              ],
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
--- packages/app/features/session/lib/src/ui/sessions_page.dart
+++ packages/app/features/session/lib/src/ui/sessions_page.dart
@@ -1,5 +1,7 @@
+import 'package:app_cores_designsystem/ui.dart';
 import 'package:flutter/material.dart';
 import 'package:packages_app_features_session/l10n.dart';
+import 'package:packages_app_features_session/src/ui/session_item.dart';
 
 class SessionsPage extends StatelessWidget {
   const SessionsPage({
@@ -9,8 +11,30 @@ class SessionsPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     final l = L10nSession.of(context);
-    return Center(
-      child: Text(l.sessionsPageTitle),
+
+    return Scaffold(
+      appBar: AppBar(
+        leading: const Center(
+          child: MainLogo(),
+        ),
+        leadingWidth: 48,
+        titleSpacing: 4,
+        centerTitle: false,
+        title: Text(l.sessionPageTitle),
+      ),
+      body: CustomScrollView(
+        slivers: [
+          SliverList(
+            delegate: SliverChildBuilderDelegate(
+              (context, index) {
+                return const SessionItem(
+                  name: 'Example Super Session Title ~ Why we using Flutter?',
+                );
+              },
+            ),
+          ),
+        ],
+      ),
     );
   }
 }
