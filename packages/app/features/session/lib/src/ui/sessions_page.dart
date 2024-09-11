ccbf71e568c2977d6e957a0c669bc067fa08e443
TK-Guitar <tk39business@gmail.com>
2024-09-11 19:48:41 +0900

    タイムラインのUIを作成しました

--- packages/app/features/session/lib/src/ui/sessions_page.dart
+++ packages/app/features/session/lib/src/ui/sessions_page.dart
@@ -1,5 +1,8 @@
+import 'dart:ffi';
+
 import 'package:app_cores_designsystem/ui.dart';
 import 'package:flutter/material.dart';
+import 'package:gap/gap.dart';
 import 'package:packages_app_features_session/l10n.dart';
 import 'package:packages_app_features_session/src/ui/session_item.dart';
 
@@ -27,9 +30,45 @@ class SessionsPage extends StatelessWidget {
           SliverList(
             delegate: SliverChildBuilderDelegate(
               (context, index) {
-                return const SessionItem(
-                  title: 'Example Super Session Title ~ Why we using Flutter?',
-                  name: 'Test',
+                return Padding(
+                  padding: const EdgeInsets.symmetric(horizontal: 8),
+                  child: Column(
+                    children: [
+                      Row(
+                        children: [
+                          Text(
+                            '11:00',
+                            style: Theme.of(context).textTheme.labelLarge,
+                          ),
+                          const Spacer(),
+                        ],
+                      ),
+                      const Gap(8),
+                      Container(
+                        margin: const EdgeInsets.only(
+                          left: 16,
+                        ),
+                        padding: const EdgeInsets.only(
+                          left: 16,
+                          bottom: 4,
+                        ),
+                        decoration: BoxDecoration(
+                          border: Border(
+                            left: BorderSide(
+                              width: 2,
+                              color:
+                                  Theme.of(context).colorScheme.outlineVariant,
+                            ),
+                          ),
+                        ),
+                        child: const SessionItem(
+                          title:
+                              'Example Super Session Title ~ Why we using Flutter?',
+                          name: 'Test',
+                        ),
+                      ),
+                    ],
+                  ),
                 );
               },
             ),
