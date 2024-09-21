55ee8cc68dd0fff6203e7da9f2fbbda5c52cc922
YumNumm <onory0917@gmail.com>
2024-09-22 03:57:26 +0900

    add: チケットカードを追加

--- apps/ticket_web/lib/pages/home/home_page.dart
+++ apps/ticket_web/lib/pages/home/home_page.dart
@@ -1,9 +1,8 @@
-import 'package:common_data/auth.dart';
-import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:go_router/go_router.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
 import 'package:ticket_web/core/components/site_scaffold.dart';
+import 'package:ticket_web/pages/home/components/ticket_cards.dart';
 import 'package:ticket_web/pages/home/components/title_and_logo.dart';
 
 part 'home_page.g.dart';
@@ -24,19 +23,14 @@ class HomePage extends ConsumerWidget {
   @override
   Widget build(BuildContext context, WidgetRef ref) {
     return SiteScaffold.widget(
-      body: Column(
+      body: const Column(
         children: [
-          const TitleAndLogo(),
-          TextButton.icon(
-            label: const Text('Sign in with Google'),
-            icon: const Icon(Icons.login),
-            onPressed: () async =>
-                ref.read(authRepositoryProvider).signInWithGoogle(
-                      redirectTo: kIsWeb
-                          ? null
-                          : 'jp.flutterkaigi.ticket://login-callback',
-                    ),
+          Padding(
+            padding: EdgeInsets.all(16),
+            child: TitleAndLogo(),
           ),
+          SizedBox(height: 48),
+          TicketCards(),
         ],
       ),
     );
