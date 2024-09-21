4c8ec2fa5f1c4da2378f9a1485f07a427d074722
YumNumm <onory0917@gmail.com>
2024-09-21 16:41:58 +0900

    add: デバッグ用にボタンを追加

--- apps/ticket_web/lib/pages/home/home_page.dart
+++ apps/ticket_web/lib/pages/home/home_page.dart
@@ -1,5 +1,8 @@
+import 'package:common_data/auth.dart';
 import 'package:flutter/material.dart';
 import 'package:go_router/go_router.dart';
+import 'package:hooks_riverpod/hooks_riverpod.dart';
+import 'package:ticket_web/feature/auth/data/current_user.dart';
 
 part 'home_page.g.dart';
 
@@ -13,14 +16,29 @@ class HomeRoute extends GoRouteData {
   }
 }
 
-class HomePage extends StatelessWidget {
+class HomePage extends ConsumerWidget {
   const HomePage({super.key});
 
   @override
-  Widget build(BuildContext context) {
-    return const Scaffold(
+  Widget build(BuildContext context, WidgetRef ref) {
+    return Scaffold(
       body: Center(
-        child: Text('Home'),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            TextButton.icon(
+              label: const Text('Sign in with Google'),
+              icon: const Icon(Icons.login),
+              onPressed: () async {
+                await ref.read(authRepositoryProvider).signInWithGoogle();
+              },
+            ),
+            const SizedBox(height: 16),
+            Text(
+              ref.watch(currentUserProvider).toString(),
+            ),
+          ],
+        ),
       ),
     );
   }
