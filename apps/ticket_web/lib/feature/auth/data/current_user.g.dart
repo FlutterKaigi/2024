255f4595e50b75367f7dd8497eb6e1827bc9ae33
YumNumm <onory0917@gmail.com>
2024-09-21 16:41:42 +0900

    regenerate code

--- apps/ticket_web/lib/feature/auth/data/current_user.g.dart
+++ apps/ticket_web/lib/feature/auth/data/current_user.g.dart
@@ -6,11 +6,11 @@ part of 'current_user.dart';
 // RiverpodGenerator
 // **************************************************************************
 
-String _$currentUserHash() => r'6083094d851c48a924943b3415426df428255e88';
+String _$currentUserHash() => r'74d481b5fd4c228e198370fcf1738b70724b1e91';
 
 /// See also [currentUser].
 @ProviderFor(currentUser)
-final currentUserProvider = Provider<User?>.internal(
+final currentUserProvider = StreamProvider<User?>.internal(
   currentUser,
   name: r'currentUserProvider',
   debugGetCreateSourceHash:
@@ -19,6 +19,6 @@ final currentUserProvider = Provider<User?>.internal(
   allTransitiveDependencies: null,
 );
 
-typedef CurrentUserRef = ProviderRef<User?>;
+typedef CurrentUserRef = StreamProviderRef<User?>;
 // ignore_for_file: type=lint
 // ignore_for_file: subtype_of_sealed_class, invalid_use_of_internal_member, invalid_use_of_visible_for_testing_member
