ec2e2b8e9c44b173437a12997954fba1f4fa455b
YumNumm <onory0917@gmail.com>
2024-09-21 15:34:03 +0900

    remove: 不要なauthRepositoryを削除

--- apps/ticket_web/lib/feature/auth/data/auth_repository.dart
+++
@@ -1,9 +0,0 @@
-import 'package:common_data/auth.dart';
-import 'package:riverpod_annotation/riverpod_annotation.dart';
-
-part 'auth_repository.g.dart';
-
-@Riverpod(keepAlive: true)
-AuthRepository authRepository(AuthRepositoryRef ref) {
-  return AuthRepository(client: ref.watch(supabaseProvider));
-}
