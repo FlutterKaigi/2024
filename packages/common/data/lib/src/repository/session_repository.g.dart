e2ea0f4469335c5be48b7e1eb1ce3af3a12cb060
YumNumm <onory0917@gmail.com>
2024-09-22 23:29:36 +0900

    add: sessionRepository

--- packages/common/data/lib/src/repository/session_repository.g.dart
+++ packages/common/data/lib/src/repository/session_repository.g.dart
@@ -23,5 +23,39 @@ final sessionRepositoryProvider = Provider<SessionRepository>.internal(
 );
 
 typedef SessionRepositoryRef = ProviderRef<SessionRepository>;
+String _$sessionVenueRepositoryHash() =>
+    r'3497b6a0fcadf2a076d1db8cd77bc2b13ea839dd';
+
+/// See also [sessionVenueRepository].
+@ProviderFor(sessionVenueRepository)
+final sessionVenueRepositoryProvider =
+    Provider<SessionVenueRepository>.internal(
+  sessionVenueRepository,
+  name: r'sessionVenueRepositoryProvider',
+  debugGetCreateSourceHash: const bool.fromEnvironment('dart.vm.product')
+      ? null
+      : _$sessionVenueRepositoryHash,
+  dependencies: null,
+  allTransitiveDependencies: null,
+);
+
+typedef SessionVenueRepositoryRef = ProviderRef<SessionVenueRepository>;
+String _$sessionSpeakerRepositoryHash() =>
+    r'14966bf7b3105c909f9b751b3aa38f57754431a0';
+
+/// See also [sessionSpeakerRepository].
+@ProviderFor(sessionSpeakerRepository)
+final sessionSpeakerRepositoryProvider =
+    Provider<SessionSpeakerRepository>.internal(
+  sessionSpeakerRepository,
+  name: r'sessionSpeakerRepositoryProvider',
+  debugGetCreateSourceHash: const bool.fromEnvironment('dart.vm.product')
+      ? null
+      : _$sessionSpeakerRepositoryHash,
+  dependencies: null,
+  allTransitiveDependencies: null,
+);
+
+typedef SessionSpeakerRepositoryRef = ProviderRef<SessionSpeakerRepository>;
 // ignore_for_file: type=lint
 // ignore_for_file: subtype_of_sealed_class, invalid_use_of_internal_member, invalid_use_of_visible_for_testing_member
