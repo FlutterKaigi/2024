5b4d1005efce103066f5c1bd30407afc7aaf99f1
YumNumm <onory0917@gmail.com>
2024-09-23 01:01:18 +0900

    refactor: SnsTypeからURLを生成する部分をEnumへ移管

--- packages/common/data/lib/src/model/sns.dart
+++ packages/common/data/lib/src/model/sns.dart
@@ -12,6 +12,18 @@ enum SnsType {
   zenn,
   note,
   other,
+  ;
+
+  String toUrl(String value) => switch (this) {
+        SnsType.github => 'https://github.com/$value',
+        SnsType.x => 'https://x.com/$value',
+        SnsType.discord => 'https://discord.com/users/$value',
+        SnsType.medium => 'https://medium.com/@$value',
+        SnsType.qiita => 'https://qiita.com/$value',
+        SnsType.zenn => 'https://zenn.dev/$value',
+        SnsType.note => 'https://note.com/$value',
+        SnsType.other => value,
+      };
 }
 
 @freezed
