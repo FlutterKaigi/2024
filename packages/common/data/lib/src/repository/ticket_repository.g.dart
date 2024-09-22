d819f5884c97bc16d05dd114f15654b363bd2d5c
YumNumm <onory0917@gmail.com>
2024-09-23 02:27:19 +0900

    add: ticketRepository

---
+++ packages/common/data/lib/src/repository/ticket_repository.g.dart
@@ -0,0 +1,27 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+// ignore_for_file: type=lint, duplicate_ignore
+
+part of 'ticket_repository.dart';
+
+// **************************************************************************
+// RiverpodGenerator
+// **************************************************************************
+
+String _$ticketRepositoryHash() => r'89311d7fa854a2f1311c88b0a667d94746f678f7';
+
+/// See also [ticketRepository].
+@ProviderFor(ticketRepository)
+final ticketRepositoryProvider = Provider<TicketRepository>.internal(
+  ticketRepository,
+  name: r'ticketRepositoryProvider',
+  debugGetCreateSourceHash: const bool.fromEnvironment('dart.vm.product')
+      ? null
+      : _$ticketRepositoryHash,
+  dependencies: null,
+  allTransitiveDependencies: null,
+);
+
+typedef TicketRepositoryRef = ProviderRef<TicketRepository>;
+// ignore_for_file: type=lint
+// ignore_for_file: subtype_of_sealed_class, invalid_use_of_internal_member, invalid_use_of_visible_for_testing_member
