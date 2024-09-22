653ef21bdf72e226633cf0ccc77e95db380e8ba0
YumNumm <onory0917@gmail.com>
2024-09-23 00:31:18 +0900

    add: export

---
+++ packages/common/data/lib/session.dart
@@ -0,0 +1,13 @@
+export 'src/model/session.dart' show Session;
+export 'src/model/session_speaker.dart' show SessionSpeaker;
+export 'src/model/session_venue.dart' show SessionVenue;
+export 'src/model/view/session_venues_with_sessions.dart'
+    show SessionVenuesWithSessions;
+export 'src/repository/session_repository.dart'
+    show
+        SessionRepository,
+        SessionSpeakerRepository,
+        SessionVenueRepository,
+        sessionRepositoryProvider,
+        sessionSpeakerRepositoryProvider,
+        sessionVenueRepositoryProvider;
