eb31985246bb71b44b3ab23e18283dcde27f7105
YumNumm <onory0917@gmail.com>
2024-09-23 01:38:06 +0900

    add: profile export file

---
+++ packages/common/data/lib/profile.dart
@@ -0,0 +1,6 @@
+export 'src/model/profile.dart' show Profile, Role;
+export 'src/model/profile_social_networking_service.dart'
+    show ProfileSocialNetworkingService;
+export 'src/model/view/profile_with_sns.dart' show $ProfileWithSnsCopyWith;
+export 'src/repository/profile_repository.dart'
+    show PagingResult, ProfileRepository;
