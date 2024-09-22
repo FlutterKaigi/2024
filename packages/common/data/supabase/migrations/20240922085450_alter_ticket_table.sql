2942e2d503364f062814fd2260952f40556a640a
YumNumm <73390859+YumNumm@users.noreply.github.com>
2024-09-22 09:36:49 +0000

    Auto format for SQL files

--- packages/common/data/supabase/migrations/20240922085450_alter_ticket_table.sql
+++ packages/common/data/supabase/migrations/20240922085450_alter_ticket_table.sql
@@ -1,7 +1,10 @@
 -- 関連するセッションを紐づける
-ALTER TABLE tickets ADD COLUMN session_id UUID REFERENCES sessions(id) ON DELETE SET NULL;
+ALTER TABLE tickets
+ADD COLUMN session_id UUID REFERENCES sessions (id) ON DELETE SET NULL;
 
 -- 関連するスポンサーを紐づける
-ALTER TABLE tickets ADD COLUMN sponsor_id SMALLINT REFERENCES sponsors(id) ON DELETE SET NULL;
+ALTER TABLE tickets
+ADD COLUMN sponsor_id smallint REFERENCES sponsors (id) ON DELETE SET NULL;
 
-ALTER TABLE tickets ADD COLUMN stripe_checkout_session_id TEXT;
+ALTER TABLE tickets
+ADD COLUMN stripe_checkout_session_id text;
