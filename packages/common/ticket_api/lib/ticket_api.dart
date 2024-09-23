bfb4f01b706d7e74dd40be212b83190fb9287fa1
YumNumm <onory0917@gmail.com>
2024-09-24 00:53:08 +0900

    add: endpoint

--- packages/common/ticket_api/lib/ticket_api.dart
+++ packages/common/ticket_api/lib/ticket_api.dart
@@ -1,5 +1,8 @@
 import 'package:dio/dio.dart';
 import 'package:retrofit/retrofit.dart';
+import 'package:ticket_api/model/ticket/get_promotion_response.dart';
+import 'package:ticket_api/model/ticket/post_promotion_request.dart';
+import 'package:ticket_api/model/ticket/post_promotion_response.dart';
 import 'package:ticket_api/model/verify_purchase_response.dart';
 
 part 'ticket_api.g.dart';
@@ -15,5 +18,16 @@ abstract class TicketApiClient {
   Future<VerifyPurchaseResponse> verifyPurchase({
     @Query('session_id') required String sessionId,
     @Header('Authorization') required String authorization,
+  });
+
+  @GET('/v1/promotion')
+  Future<GetPromotionResponse> getPromotion({
+    @Query('code') required String code,
+  });
+
+  @POST('/v1/promotion')
+  Future<PostPromotionResponse> postPromotion({
+    @Header('Authorization') required String authorization,
+    @Body() required PostPromotionRequest body,
   });
 }
