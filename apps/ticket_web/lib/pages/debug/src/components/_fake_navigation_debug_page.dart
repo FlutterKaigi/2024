fe97e0e5fec4036d2efa78ef49afca994c79d9b7
YumNumm <onory0917@gmail.com>
2024-09-30 22:43:32 +0900

    add: pages

---
+++ apps/ticket_web/lib/pages/debug/src/components/_fake_navigation_debug_page.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class NavigationDebugPage extends StatelessWidget {
+  const NavigationDebugPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    throw UnimplementedError('kDebugModeがtrueの時のみ利用できます');
+  }
+}
