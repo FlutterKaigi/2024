4af92cd75703e6feeede3b57f1d7a76c33e352ae
YumNumm <onory0917@gmail.com>
2024-10-05 09:14:56 +0900

    fix: 日付で販売開始の出し分け (Stripe側で最終的な発売開始をハンドルします)

--- apps/ticket_web/lib/app.dart
+++ apps/ticket_web/lib/app.dart
@@ -1,4 +1,3 @@
-import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_hooks/flutter_hooks.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
@@ -57,20 +56,19 @@ class App extends HookConsumerWidget {
             child: Stack(
               children: [
                 if (child != null) child,
-                if (kDebugMode)
-                  Align(
-                    alignment: Alignment.bottomRight,
-                    child: Padding(
-                      padding: const EdgeInsets.all(8),
-                      child: FloatingActionButton.small(
-                        onPressed: () async =>
-                            router.push(const DebugRoute().location),
-                        child: const Icon(
-                          Icons.bug_report_outlined,
-                        ),
+                Align(
+                  alignment: Alignment.bottomRight,
+                  child: Padding(
+                    padding: const EdgeInsets.all(8),
+                    child: FloatingActionButton.small(
+                      onPressed: () async =>
+                          router.push(const DebugRoute().location),
+                      child: const Icon(
+                        Icons.bug_report_outlined,
                       ),
                     ),
                   ),
+                ),
               ],
             ),
           );
--- apps/ticket_web/lib/pages/home/home_page.dart
+++ apps/ticket_web/lib/pages/home/home_page.dart
@@ -3,6 +3,7 @@ import 'package:hooks_riverpod/hooks_riverpod.dart';
 import 'package:ticket_web/core/components/error/error_card.dart';
 import 'package:ticket_web/core/components/language_selector.dart';
 import 'package:ticket_web/core/components/site_scaffold.dart';
+import 'package:ticket_web/core/provider/environment.dart';
 import 'package:ticket_web/core/router/router.dart';
 import 'package:ticket_web/feature/profile/ui/app_bar_avatar.dart';
 import 'package:ticket_web/feature/ticket/data/ticket_notifier.dart';
@@ -11,6 +12,7 @@ import 'package:ticket_web/pages/home/components/already_purchased_card.dart';
 import 'package:ticket_web/pages/home/components/auth_state_card.dart';
 import 'package:ticket_web/pages/home/components/ticket_cards.dart';
 import 'package:ticket_web/pages/home/components/ticket_cards/information_for_students.dart';
+import 'package:ticket_web/pages/home/components/ticket_will_be_on_sale_card.dart';
 import 'package:ticket_web/pages/home/components/title_and_logo.dart';
 import 'package:ticket_web/pages/home/components/transit_to_home_page.dart';
 import 'package:ticket_web/pages/ticket/ticket_page.dart';
@@ -22,6 +24,14 @@ class HomePage extends ConsumerWidget {
   Widget build(BuildContext context, WidgetRef ref) {
     final ticketState = ref.watch(ticketNotifierProvider);
     final i18n = Translations.of(context);
+
+    // 2024/10/07 10:00 JST から販売開始
+    final isTicketOnSale = DateTime.now()
+            .toUtc()
+            .add(const Duration(hours: 9))
+            .isAfter(DateTime.utc(2024, 10, 7, 10)) ||
+        ref.watch(environmentProvider).environmentType !=
+            EnvironmentType.production;
 
     return SiteScaffold.widget(
       actions: const [
@@ -37,7 +47,10 @@ class HomePage extends ConsumerWidget {
           ),
           const TransitToHomePage(),
           const SizedBox(height: 48),
-          const AuthStateCard(),
+          if (isTicketOnSale)
+            const AuthStateCard()
+          else
+            const TicketWillBeOnSaleCard(),
           ticketState.when(
             data: (ticket) {
               if (ticket != null) {
