0758d30d856e157d98f7d1a626d133baf5c0ad25
YumNumm <onory0917@gmail.com>
2024-10-07 00:05:21 +0900

    fix: プラットフォームごとに分離

--- apps/ticket_web/lib/core/provider/image_picker_provider.dart
+++ apps/ticket_web/lib/core/provider/image_picker_provider.dart
@@ -1,7 +1,3 @@
-import 'package:image_picker_for_web/image_picker_for_web.dart';
-import 'package:riverpod_annotation/riverpod_annotation.dart';
-
-part 'image_picker_provider.g.dart';
-
-@Riverpod(keepAlive: true)
-ImagePickerPlugin imagePicker(ImagePickerRef ref) => ImagePickerPlugin();
+export './image_picker/image_picker_for_io.dart'
+    if (dart.library.js_interop) './image_picker/image_picker_for_web.dart'
+    show ImagePickerRef, imagePicker, imagePickerProvider;
