209b84f9f8ca722e2658ea67c83d5c2a66f280fe
Tatsuya Okayama (blendthink) <admin@blendthink.dev>
2024-10-07 00:29:47 +0900

    fix: macOS でビルド可能に修正

---
+++ apps/ticket_web/lib/feature/ticket/ui/components/crop_widget/crop_widget_io.dart
@@ -0,0 +1,39 @@
+import 'dart:typed_data';
+
+import 'package:crop_your_image/crop_your_image.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_hooks/flutter_hooks.dart';
+import 'package:ticket_web/feature/ticket/ui/workers/resize.dart';
+
+class CropWidget extends HookWidget {
+  const CropWidget({
+    required this.imageBytes,
+    required this.isCropping,
+    required this.controller,
+    super.key,
+  });
+
+  final Uint8List imageBytes;
+  final ValueNotifier<bool> isCropping;
+  final CropController controller;
+
+  @override
+  Widget build(BuildContext context) {
+    return Crop(
+      controller: controller,
+      image: imageBytes,
+      onCropped: (croppedImage) async {
+        final resizedImageBytes = await resize(croppedImage);
+        if (context.mounted) {
+          isCropping.value = false;
+          Navigator.of(context).pop(resizedImageBytes);
+        }
+      },
+      aspectRatio: 1,
+      withCircleUi: true,
+      progressIndicator: const Center(
+        child: CircularProgressIndicator.adaptive(),
+      ),
+    );
+  }
+}
---
+++ apps/ticket_web/lib/feature/ticket/ui/components/crop_widget/crop_widget_web.dart
@@ -0,0 +1,59 @@
+import 'dart:js_interop';
+import 'dart:typed_data';
+
+import 'package:crop_your_image/crop_your_image.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_hooks/flutter_hooks.dart';
+import 'package:web/web.dart' as web;
+
+class BaseCropWidget extends HookWidget {
+  const BaseCropWidget({
+    required this.imageBytes,
+    required this.isCropping,
+    required this.controller,
+    super.key,
+  });
+
+  final Uint8List imageBytes;
+  final ValueNotifier<bool> isCropping;
+  final CropController controller;
+
+  @override
+  Widget build(BuildContext context) {
+    final worker = useMemoized(
+      () => web.Worker(
+        'assets/workers/resize_worker.js'.toJS,
+      ),
+      [],
+    );
+
+    useEffect(
+      () {
+        worker.onmessage = ((web.MessageEvent message) {
+          final resizedImageBytes =
+              (message.data as JSUint8Array?)?.toDart ?? Uint8List.fromList([]);
+          if (context.mounted) {
+            isCropping.value = false;
+            Navigator.of(context).pop(resizedImageBytes);
+          }
+        }).toJS;
+        return null;
+      },
+      [worker],
+    );
+
+    return Crop(
+      controller: controller,
+      image: imageBytes,
+      onCropped: (croppedImage) async {
+        // Web では Worker を使用して画像をリサイズする
+        worker.postMessage(croppedImage.toJS);
+      },
+      aspectRatio: 1,
+      withCircleUi: true,
+      progressIndicator: const Center(
+        child: CircularProgressIndicator.adaptive(),
+      ),
+    );
+  }
+}
