f0bafbf728f240e7effdc151449fc7dfcdb75bc3
Kurogoma4D <highdro0479@gmail.com>
2024-10-13 18:23:30 +0900

    Change app label on iOS

---
+++ apps/app/ios/scripts/extract_dart_defines.sh
@@ -0,0 +1,17 @@
+#!/bin/sh
+
+OUTPUT_FILE="${SRCROOT}/Flutter/Dart-Defines.xcconfig"
+: > $OUTPUT_FILE
+
+function decode_url() { echo "${*}" | base64 --decode; }
+
+IFS=',' read -r -a define_items <<<"$DART_DEFINES"
+
+for index in "${!define_items[@]}"
+do
+    item=$(decode_url "${define_items[$index]}")
+    lowercase_item=$(echo "$item" | tr '[:upper:]' '[:lower:]')
+    if [[ $lowercase_item != flutter* ]]; then
+        echo "$item" >> "$OUTPUT_FILE"
+    fi
+done
