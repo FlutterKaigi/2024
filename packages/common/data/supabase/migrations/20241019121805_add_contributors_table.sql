10e3567f6b64fd6ab27fdfbb2bbe6d661b224f76
Tatsuya Okayama (blendthink) <admin@blendthink.dev>
2024-10-20 10:36:24 +0900

    feature: contributors テーブルを追加

---
+++ packages/common/data/supabase/migrations/20241019121805_add_contributors_table.sql
@@ -0,0 +1,19 @@
+CREATE TABLE IF NOT EXISTS contributors (
+    id UUID NOT NULL PRIMARY KEY DEFAULT uuid_generate_v4 (),
+    name text NOT NULL,
+    avatar_url text NOT NULL,
+    contribution_count smallserial NOT NULL,
+    UNIQUE (name)
+);
+
+ALTER TABLE contributors enable ROW level security;
+
+CREATE POLICY "Everyone can read contributors" ON contributors FOR
+SELECT
+    USING (TRUE);
+
+CREATE POLICY "Admin can insert contributors" ON contributors FOR insert TO authenticated
+WITH
+    CHECK (role () = 'admin');
+
+CREATE POLICY "Admin can delete contributors" ON contributors FOR delete USING (role () = 'admin');
