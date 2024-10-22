92d5b6eb720b8fcb9e30dba2f178a4b4b7ba1ec2
Tatsuya Okayama (blendthink) <admin@blendthink.dev>
2024-10-22 20:53:53 +0900

    refactor: newsProvider をcommon_data パッケージへ移動

---
+++ packages/common/data/lib/src/provider/news_provider.g.dart
@@ -0,0 +1,26 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+// ignore_for_file: type=lint, duplicate_ignore
+
+part of 'news_provider.dart';
+
+// **************************************************************************
+// RiverpodGenerator
+// **************************************************************************
+
+String _$newsHash() => r'6a1722785ef9ea715270b1b8613e5c5b196b3e56';
+
+/// See also [news].
+@ProviderFor(news)
+final newsProvider = AutoDisposeFutureProvider<List<News>>.internal(
+  news,
+  name: r'newsProvider',
+  debugGetCreateSourceHash:
+      const bool.fromEnvironment('dart.vm.product') ? null : _$newsHash,
+  dependencies: null,
+  allTransitiveDependencies: null,
+);
+
+typedef NewsRef = AutoDisposeFutureProviderRef<List<News>>;
+// ignore_for_file: type=lint
+// ignore_for_file: subtype_of_sealed_class, invalid_use_of_internal_member, invalid_use_of_visible_for_testing_member
