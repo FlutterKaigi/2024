eec4630f098a6af6e70e7a7d2873211e12bcc707
Kurogoma4D <highdro0479@gmail.com>
2024-10-26 18:40:15 +0900

    Add model and provider

---
+++ packages/app/features/session/lib/src/providers/session_timeline.dart
@@ -0,0 +1,63 @@
+import 'package:common_data/session.dart';
+import 'package:common_data/sponsor.dart';
+import 'package:freezed_annotation/freezed_annotation.dart';
+import 'package:riverpod/riverpod.dart';
+import 'package:riverpod_annotation/riverpod_annotation.dart';
+
+part 'session_timeline.freezed.dart';
+
+@riverpod
+Future<List<TimelineItem>> sessionTimeline(Ref ref) async {
+  final repository = ref.watch(sessionRepositoryProvider);
+  final sessionVenuesWithSessions =
+      await repository.fetchSessionVenuesWithSessions();
+
+  final timelineItems = <TimelineItem>[];
+  for (final venueWithSessions in sessionVenuesWithSessions) {
+    final venue = SessionVenue(
+      id: venueWithSessions.id,
+      name: venueWithSessions.name,
+    );
+
+    final sessions = venueWithSessions.sessions
+      ..sort((a, b) => a.startsAt.compareTo(b.startsAt));
+    for (final session in sessions) {
+      timelineItems.add(
+        TimelineItem.session(
+          id: session.id,
+          title: session.title,
+          description: session.description,
+          startsAt: session.startsAt,
+          endsAt: session.endsAt,
+          isLightningTalk: session.isLightningTalk,
+          venue: venue,
+          speakers: session.speakers,
+          sponsors: session.sponsors,
+        ),
+      );
+    }
+  }
+
+  return timelineItems;
+}
+
+@freezed
+class TimelineItem with _$TimelineItem {
+  const factory TimelineItem.event({
+    required String title,
+    required DateTime startsAt,
+    required DateTime endsAt,
+  }) = _TimelineItemEvent;
+
+  const factory TimelineItem.session({
+    required String id,
+    required String title,
+    required String description,
+    required DateTime startsAt,
+    required DateTime endsAt,
+    required bool isLightningTalk,
+    required SessionVenue venue,
+    required List<ProfileWithSns> speakers,
+    required List<Sponsor> sponsors,
+  }) = _TimelineItemSession;
+}
