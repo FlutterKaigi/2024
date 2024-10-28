220271263352e55bda8a64a4b1a1669dda5fe086
YumNumm <onory0917@gmail.com>
2024-10-29 01:31:09 +0900

    add: jobBoardFutureProvider

---
+++ apps/website/lib/feature/job_board/data/job_board_notifier.g.dart
@@ -0,0 +1,26 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'job_board_notifier.dart';
+
+// **************************************************************************
+// RiverpodGenerator
+// **************************************************************************
+
+String _$jobBoardHash() => r'9de9aba20eda0da9d7bf9a3308d942f484fd71f5';
+
+/// See also [jobBoard].
+@ProviderFor(jobBoard)
+final jobBoardProvider = FutureProvider<List<JobBoard>>.internal(
+  jobBoard,
+  name: r'jobBoardProvider',
+  debugGetCreateSourceHash:
+      const bool.fromEnvironment('dart.vm.product') ? null : _$jobBoardHash,
+  dependencies: null,
+  allTransitiveDependencies: null,
+);
+
+@Deprecated('Will be removed in 3.0. Use Ref instead')
+// ignore: unused_element
+typedef JobBoardRef = FutureProviderRef<List<JobBoard>>;
+// ignore_for_file: type=lint
+// ignore_for_file: subtype_of_sealed_class, invalid_use_of_internal_member, invalid_use_of_visible_for_testing_member, deprecated_member_use_from_same_package
