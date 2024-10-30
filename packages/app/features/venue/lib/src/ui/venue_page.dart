6ee69ecd1a400c039e87e13b41391e504828dc3d
techkeito <techkeito@gmail.com>
2024-10-30 17:39:33 +0900

    feat: フロアガイドを追加

--- packages/app/features/venue/lib/src/ui/venue_page.dart
+++ packages/app/features/venue/lib/src/ui/venue_page.dart
@@ -1,4 +1,5 @@
-import 'package:app_cores_core/ui.dart';
+import 'package:app_features_venue/l10n.dart';
+import 'package:app_features_venue/src/gen/assets/assets.gen.dart';
 import 'package:flutter/material.dart';
 
 class VenuePage extends StatelessWidget {
@@ -8,8 +9,25 @@ class VenuePage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const ComingSoonCover(
-      child: SizedBox.shrink(),
+    final l = L10nVenue.of(context);
+
+    return Center(
+      child: Padding(
+        padding: const EdgeInsets.symmetric(
+          vertical: 16,
+          horizontal: 16,
+        ),
+        child: SizedBox(
+          width: 1000,
+          height: 800,
+          child: InteractiveViewer(
+            minScale: 1,
+            maxScale: 3,
+            child: Assets.images.floorMap
+                .image(width: 48, height: 48, semanticLabel: l.venueFloorMap),
+          ),
+        ),
+      ),
     );
   }
 }
