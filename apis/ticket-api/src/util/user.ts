b6f30945e51caae528e4132820420c31f1f035f4
YumNumm <onory0917@gmail.com>
2024-11-03 04:30:36 +0900

    fix: format

--- apis/ticket-api/src/util/user.ts
+++ apis/ticket-api/src/util/user.ts
@@ -1,48 +1,48 @@
-import { type SupabaseClient, type User } from "@supabase/supabase-js";
+import type { SupabaseClient, User } from "@supabase/supabase-js";
 import type { Database } from "supabase-types";
 
 export async function getUser(
-  authorizationHeader: string,
-  supabase: SupabaseClient<Database>
+	authorizationHeader: string,
+	supabase: SupabaseClient<Database>,
 ): Promise<User | null> {
-  const jwt = authorizationHeader.replace("Bearer ", "");
-  console.log(jwt);
-  const {
-    data: { user }
-  } = await supabase.auth.getUser(jwt);
+	const jwt = authorizationHeader.replace("Bearer ", "");
+	console.log(jwt);
+	const {
+		data: { user },
+	} = await supabase.auth.getUser(jwt);
 
-  return user;
+	return user;
 }
 
 export async function getUserWithProfile(
-  authorizationHeader: string,
-  supabase: SupabaseClient<Database>
+	authorizationHeader: string,
+	supabase: SupabaseClient<Database>,
 ): Promise<
-  | {
-      success: true;
-      user: User;
-      profile: Database["public"]["Tables"]["profiles"]["Row"];
-    }
-  | {
-      success: false;
-      error: string;
-    }
+	| {
+			success: true;
+			user: User;
+			profile: Database["public"]["Tables"]["profiles"]["Row"];
+	  }
+	| {
+			success: false;
+			error: string;
+	  }
 > {
-  const user = await getUser(authorizationHeader, supabase);
-  if (!user) {
-    return { success: false, error: "User not found" };
-  }
-  const { data: profile, error } = await supabase
-    .from("profiles")
-    .select("*")
-    .eq("id", user.id)
-    .single();
-  if (error) {
-    return { success: false, error: error.message };
-  }
-  return {
-    success: true,
-    user,
-    profile
-  };
+	const user = await getUser(authorizationHeader, supabase);
+	if (!user) {
+		return { success: false, error: "User not found" };
+	}
+	const { data: profile, error } = await supabase
+		.from("profiles")
+		.select("*")
+		.eq("id", user.id)
+		.single();
+	if (error) {
+		return { success: false, error: error.message };
+	}
+	return {
+		success: true,
+		user,
+		profile,
+	};
 }
