9cc525d3509c77e2c818b0d183478b28121e1da5
iseruuuuu <iseki.ryutaro@rkb.bb4u.ne.jp>
2024-11-07 21:17:09 +0900

    add: アプリ用とWeb用のURLを追加しました

--- packages/app/cores/core/lib/src/constants/official_social_urls.dart
+++ packages/app/cores/core/lib/src/constants/official_social_urls.dart
@@ -1,3 +1,5 @@
 class OfficialSocialUrls {
   static const medium = 'https://medium.com/flutterkaigi';
+  static const nativeX = 'twitter://user?screen_name=FlutterKaigi';
+  static const webX = 'https://twitter.com/FlutterKaigi';
 }
