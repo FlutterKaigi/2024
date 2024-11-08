236bb89bd7986c6fb6b53b00bf2813b91f050c1d
iseruuuuu <iseki.ryutaro@rkb.bb4u.ne.jp>
2024-11-08 10:39:12 +0900

    fix: URLを修正しました

--- packages/app/cores/core/lib/src/constants/official_social_urls.dart
+++ packages/app/cores/core/lib/src/constants/official_social_urls.dart
@@ -1,5 +1,5 @@
 class OfficialSocialUrls {
   static const medium = 'https://medium.com/flutterkaigi';
   static const nativeX = 'twitter://user?screen_name=FlutterKaigi';
-  static const webX = 'https://twitter.com/FlutterKaigi';
+  static const webX = 'https://x.com/FlutterKaigi';
 }
