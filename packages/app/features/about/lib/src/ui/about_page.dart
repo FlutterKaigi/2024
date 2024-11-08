394440c91ecdf6ce73434a038afecdc294f7f341
iseruuuuu <iseki.ryutaro@rkb.bb4u.ne.jp>
2024-11-08 19:59:48 +0900

    fix: 不要なimportを削除しました

--- packages/app/features/about/lib/src/ui/about_page.dart
+++ packages/app/features/about/lib/src/ui/about_page.dart
@@ -18,7 +18,6 @@ import 'package:flutter/services.dart';
 import 'package:gap/gap.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
 import 'package:intl/intl.dart';
-import 'package:url_launcher/url_launcher.dart';
 
 class AboutPage extends ConsumerWidget {
   const AboutPage({
