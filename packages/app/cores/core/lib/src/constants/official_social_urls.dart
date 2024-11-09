01607466652d86bf548a82400a04f95d3621758c
miyasic <kou.sepak@gmail.com>
2024-11-09 20:39:25 +0900

    fix: GitHubのURLをプロジェクト指定からOrgnization指定に変更

--- packages/app/cores/core/lib/src/constants/official_social_urls.dart
+++ packages/app/cores/core/lib/src/constants/official_social_urls.dart
@@ -1,5 +1,5 @@
 class OfficialSocialUrls {
   static const medium = 'https://medium.com/flutterkaigi';
   static const x = 'https://x.com/FlutterKaigi';
-  static const github = 'https://github.com/FlutterKaigi/2024';
+  static const github = 'https://github.com/FlutterKaigi';
 }
