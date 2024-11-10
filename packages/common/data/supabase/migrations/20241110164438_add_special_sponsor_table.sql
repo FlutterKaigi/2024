4d20c0fe57c6b132ed6be432949764978cf91614
YumNumm <onory0917@gmail.com>
2024-11-11 02:21:12 +0900

    fix: テーブル名を修正

--- packages/common/data/supabase/migrations/20241110164438_add_special_sponsor_table.sql
+++ packages/common/data/supabase/migrations/20241110164438_add_special_sponsor_table.sql
@@ -1,6 +1,6 @@
 CREATE TYPE public.special_sponsor_type AS enum('community', 'translation');
 
-CREATE TABLE public.sponsors (
+CREATE TABLE public.special_sponsors (
   id smallserial NOT NULL PRIMARY KEY,
   name text NOT NULL,
   logo_name text NOT NULL, -- https://[project_id].supabase.co/storage/v1/object/public/sponsors/[logo_name]
@@ -10,10 +10,10 @@ CREATE TABLE public.sponsors (
   sort_id smallint NOT NULL UNIQUE
 );
 
-ALTER TABLE public.sponsors enable ROW level security;
+ALTER TABLE public.special_sponsors enable ROW level security;
 
-CREATE POLICY "Everyone can read sponsors" ON public.sponsors FOR
+CREATE POLICY "Everyone can read special_sponsors" ON public.special_sponsors FOR
 SELECT
   USING (TRUE);
 
-CREATE POLICY "Admin can CRUD sponsors" ON public.sponsors FOR ALL TO authenticated USING (role () = 'admin');
+CREATE POLICY "Admin can CRUD special_sponsors" ON public.special_sponsors FOR ALL TO authenticated USING (role () = 'admin');
