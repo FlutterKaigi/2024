183cba637a96d19d85492d5a33aae3617f826498
Tomoki Kobayashi <temoki@gmail.com>
2024-11-11 08:14:38 +0900

    Fix comment

--- packages/common/data/lib/src/repository/news_repository.dart
+++ packages/common/data/lib/src/repository/news_repository.dart
@@ -12,9 +12,9 @@ NewsRepository newsRepository(Ref ref) => NewsRepositoryImpl(
     );
 
 abstract interface class NewsRepository {
-  /// 掲載中のお知らせを取得する
+  /// 有効なお知らせを取得する
   ///
-  /// 掲載中のお知らせの条件
+  /// 有効なお知らせの条件
   /// - お知らせがすでに始まっている（開始時刻が現在と等しいか、現在より前）。
   /// - お知らせがまだ終了していない（終了時刻が現在より後、もしくは終了時刻が指定されていない）。
   Future<List<News>> getNews();
