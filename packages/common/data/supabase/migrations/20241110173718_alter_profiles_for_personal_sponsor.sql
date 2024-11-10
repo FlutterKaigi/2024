a1f128beb9efa113ee5bee4359895932619cef12
YumNumm <onory0917@gmail.com>
2024-11-11 04:22:53 +0900

    fix: 検査項目を追加

--- packages/common/data/supabase/migrations/20241110173718_alter_profiles_for_personal_sponsor.sql
+++ packages/common/data/supabase/migrations/20241110173718_alter_profiles_for_personal_sponsor.sql
@@ -8,8 +8,11 @@ BEGIN
   RETURN (
     SELECT 1
     FROM public.profiles as p
-    WHERE p.id = search_user_id
-    AND is_published = TRUE
+    WHERE p.id = search_user_id AND is_published = TRUE AND (
+      SELECT 1
+      FROM public.tickets as t
+      WHERE t.user_id = search_user_id AND t.type = 'individual_sponsor'::ticket_type
+    )::boolean
   );
 END;
 $$;
@@ -17,9 +20,7 @@ $$;
 -- 新しいポリシーを作成
 CREATE POLICY "Everyone can read individual_sponsor in profiles" ON public.profiles FOR
 SELECT
-  USING (
-    public.is_profile_published_as_individual_sponsor (id)
-  );
+  USING (public.is_profile_published_as_individual_sponsor (id));
 
 CREATE POLICY "Everyone can read individual_sponsor in profile_social_networking_services" ON public.profile_social_networking_services FOR
 SELECT
