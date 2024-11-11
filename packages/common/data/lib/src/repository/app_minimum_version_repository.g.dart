393140e29efeb1846b4e27a563d57c7ea41724fc
Tatsuya Okayama (blendthink) <admin@blendthink.dev>
2024-11-11 13:54:30 +0900

    feat: AppMinimumVersionRepository クラスとその Provider を作成

---
+++ packages/common/data/lib/src/repository/app_minimum_version_repository.g.dart
@@ -0,0 +1,32 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+// ignore_for_file: type=lint, duplicate_ignore
+
+part of 'app_minimum_version_repository.dart';
+
+// **************************************************************************
+// RiverpodGenerator
+// **************************************************************************
+
+String _$appMinimumVersionRepositoryHash() =>
+    r'bd123d561a917ca2a9fd3792dd2a810f2a5ae5f5';
+
+/// See also [appMinimumVersionRepository].
+@ProviderFor(appMinimumVersionRepository)
+final appMinimumVersionRepositoryProvider =
+    Provider<AppMinimumVersionRepository>.internal(
+  appMinimumVersionRepository,
+  name: r'appMinimumVersionRepositoryProvider',
+  debugGetCreateSourceHash: const bool.fromEnvironment('dart.vm.product')
+      ? null
+      : _$appMinimumVersionRepositoryHash,
+  dependencies: null,
+  allTransitiveDependencies: null,
+);
+
+@Deprecated('Will be removed in 3.0. Use Ref instead')
+// ignore: unused_element
+typedef AppMinimumVersionRepositoryRef
+    = ProviderRef<AppMinimumVersionRepository>;
+// ignore_for_file: type=lint
+// ignore_for_file: subtype_of_sealed_class, invalid_use_of_internal_member, invalid_use_of_visible_for_testing_member, deprecated_member_use_from_same_package
