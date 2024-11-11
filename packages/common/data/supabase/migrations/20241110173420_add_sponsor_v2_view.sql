3ab32623af19844db210640075a34b3509fab7f4
YumNumm <onory0917@gmail.com>
2024-11-12 01:44:08 +0900

    add: `special_sponsor`に対応した`session_venues_with_sessions_v3`を作成

--- packages/common/data/supabase/migrations/20241110173420_add_sponsor_v2_view.sql
+++ packages/common/data/supabase/migrations/20241110173420_add_sponsor_v2_view.sql
@@ -85,3 +85,70 @@ SELECT
 FROM
   sponsors_v2 sp
   LEFT JOIN session_details sd ON sd.sponsor_id = sp.id;
+
+CREATE VIEW public.session_venues_with_sessions_v3
+WITH
+  (security_invoker = TRUE) AS
+WITH
+  speaker_details AS (
+    SELECT
+      ss.session_id,
+      json_agg(sp.*) AS speakers
+    FROM
+      session_speakers_v2 ss
+      JOIN speakers sp ON ss.speaker_id = sp.id
+    GROUP BY
+      ss.session_id
+  ),
+  sponsor_details AS (
+    SELECT
+      s.id AS session_id,
+      json_agg(sp.*) AS sponsors
+    FROM
+      sessions s
+      JOIN sponsors_v2 sp ON sp.id = s.sponsor_id
+      -- session_venues_with_sessions_v2との変更点
+      -- sponsorsテーブルから sponsors_v2テーブルに変更
+    GROUP BY
+      s.id
+  ),
+  session_details AS (
+    SELECT
+      s.venue_id,
+      json_agg(
+        json_build_object(
+          'id',
+          s.id,
+          'title',
+          s.title,
+          'description',
+          s.description,
+          'starts_at',
+          s.starts_at,
+          'ends_at',
+          s.ends_at,
+          'is_lightning_talk',
+          s.is_lightning_talk,
+          'speakers',
+          coalesce(sd.speakers, '[]'::json),
+          'sponsors',
+          coalesce(spd.sponsors, '[]'::json)
+        )
+      ) FILTER (
+        WHERE
+          s.id IS NOT NULL
+      ) AS sessions
+    FROM
+      sessions s
+      LEFT JOIN speaker_details sd ON sd.session_id = s.id
+      LEFT JOIN sponsor_details spd ON spd.session_id = s.id
+    GROUP BY
+      s.venue_id
+  )
+SELECT
+  v.id,
+  v.name,
+  coalesce(sd.sessions, '[]'::json) AS sessions
+FROM
+  session_venues v
+  LEFT JOIN session_details sd ON sd.venue_id = v.id;
