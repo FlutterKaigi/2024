70f089389898a569b67708ff6b9c322ff020dca6
YumNumm <onory0917@gmail.com>
2024-11-13 04:51:34 +0900

    refactor: profiles_with_sns_for_individual_sponsor

--- packages/common/data/supabase/migrations/20241112193700_alter_profiles_with_sns_for_individual_sponsor.sql
+++ packages/common/data/supabase/migrations/20241112193700_alter_profiles_with_sns_for_individual_sponsor.sql
@@ -2,14 +2,10 @@ DROP VIEW public.profiles_with_sns_for_individual_sponsor;
 
 CREATE VIEW public.profiles_with_sns_for_individual_sponsor AS
 SELECT
-  p.*,
-  json_agg(psns.*) AS sns_accounts
+  p.*
 FROM
-  public.profiles AS p
-  JOIN public.tickets AS t ON t.user_id = p.id
-  JOIN public.profile_social_networking_services AS psns ON psns.id = p.id
+  public.profiles_with_sns AS p
+  LEFT JOIN public.tickets AS t ON t.user_id = p.id
 WHERE
   t.type = 'individual_sponsor'::ticket_type
   AND p.is_published = TRUE
-GROUP BY
-  p.id;
