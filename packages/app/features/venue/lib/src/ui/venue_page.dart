926697a3f6e0c4e8198f4d1e32602dbe0ab31b58
techkeito <techkeito@gmail.com>
2024-11-15 14:34:48 +0900

    feat: 言語設定に応じて、フロアマップを切り替える

--- packages/app/features/venue/lib/src/ui/venue_page.dart
+++ packages/app/features/venue/lib/src/ui/venue_page.dart
@@ -1,17 +1,35 @@
+import 'package:app_cores_designsystem/providers.dart';
 import 'package:app_cores_designsystem/ui.dart';
 import 'package:app_cores_settings/ui.dart';
 import 'package:app_features_venue/l10n.dart';
 import 'package:app_features_venue/src/gen/assets/assets.gen.dart';
 import 'package:flutter/material.dart';
+import 'package:hooks_riverpod/hooks_riverpod.dart';
 
-class VenuePage extends StatelessWidget {
+class VenuePage extends ConsumerWidget {
   const VenuePage({
     super.key,
   });
 
+  AssetGenImage _getFloorMap(BuildContext context, LocalizationMode mode) {
+    switch (mode) {
+      case LocalizationMode.system:
+        final systemLocale = Localizations.localeOf(context);
+        return systemLocale.languageCode == 'en'
+            ? Assets.images.floorMapEn
+            : Assets.images.floorMapJa;
+      case LocalizationMode.japanese:
+        return Assets.images.floorMapJa;
+      case LocalizationMode.english:
+        return Assets.images.floorMapEn;
+    }
+  }
+
   @override
-  Widget build(BuildContext context) {
+  Widget build(BuildContext context, WidgetRef ref) {
     final l = L10nVenue.of(context);
+    final localizationMode = ref.watch(localizationModeStoreProvider);
+    final floorMap = _getFloorMap(context, localizationMode);
 
     return Scaffold(
       appBar: AppBar(
@@ -38,8 +56,11 @@ class VenuePage extends StatelessWidget {
             child: InteractiveViewer(
               minScale: 1,
               maxScale: 3,
-              child: Assets.images.floorMap
-                  .image(width: 48, height: 48, semanticLabel: l.venueFloorMap),
+              child: floorMap.image(
+                width: 48,
+                height: 48,
+                semanticLabel: l.venueFloorMap,
+              ),
             ),
           ),
         ),
