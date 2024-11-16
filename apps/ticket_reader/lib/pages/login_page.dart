49993594ce526039e62eaf9b25b4ac415b115d2e
YumNumm <onory0917@gmail.com>
2024-11-17 01:47:29 +0900

    add: login background widget

--- apps/ticket_reader/lib/pages/login_page.dart
+++ apps/ticket_reader/lib/pages/login_page.dart
@@ -1,16 +1,42 @@
 import 'package:flutter/material.dart';
+import 'package:mesh_gradient/mesh_gradient.dart';
 
 class LoginPage extends StatelessWidget {
   const LoginPage({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: const Text('Login'),
+    return const Scaffold(
+      body: Stack(
+        children: [
+          _Background(),
+        ],
       ),
-      body: const Center(
-        child: Text('Login'),
+    );
+  }
+}
+
+class _Background extends StatelessWidget {
+  const _Background();
+
+  @override
+  Widget build(BuildContext context) {
+    final isDark = MediaQuery.platformBrightnessOf(context) == Brightness.dark;
+
+    return SizedBox.expand(
+      child: AnimatedMeshGradient(
+        colors: [
+          Colors.red,
+          Colors.purpleAccent,
+          Colors.blueAccent,
+          Colors.greenAccent,
+        ]
+            .map(
+              (color) =>
+                  Color.lerp(color, isDark ? Colors.black : Colors.white, 0.4)!,
+            )
+            .toList(),
+        options: AnimatedMeshGradientOptions(speed: 0.01),
       ),
     );
   }
