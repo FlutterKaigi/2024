c05b40c80e705d954e4e762dd8b269f57edd9657
YumNumm <onory0917@gmail.com>
2024-11-17 01:08:16 +0900

    fix: themeの修正

--- apps/ticket_reader/lib/core/theme/theme.dart
+++ apps/ticket_reader/lib/core/theme/theme.dart
@@ -3,11 +3,12 @@ import 'package:flutter/material.dart';
 ThemeData lightTheme = ThemeData(
   colorScheme: ColorScheme.fromSeed(seedColor: Colors.blue),
   useMaterial3: true,
-  brightness: Brightness.light,
 );
 
 ThemeData darkTheme = ThemeData(
-  colorScheme: ColorScheme.fromSeed(seedColor: Colors.blue),
+  colorScheme: ColorScheme.fromSeed(
+    seedColor: Colors.blue,
+    brightness: Brightness.dark,
+  ),
   useMaterial3: true,
-  brightness: Brightness.dark,
 );
