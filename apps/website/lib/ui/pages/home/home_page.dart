352e88c502c731b7c788fad779c939a06474f411
akaboshinit <akaboshinit@gmail.com>
2024-11-17 23:36:12 +0900

    chore: kIsWasm text

--- apps/website/lib/ui/pages/home/home_page.dart
+++ apps/website/lib/ui/pages/home/home_page.dart
@@ -13,6 +13,7 @@ import 'package:conference_2024_website/ui/pages/home/components/session.dart';
 import 'package:conference_2024_website/ui/pages/home/components/sponsors.dart';
 import 'package:conference_2024_website/ui/pages/home/components/ticket.dart';
 import 'package:conference_2024_website/ui/pages/home/components/title_and_logo.dart';
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_hooks/flutter_hooks.dart';
 import 'package:gap/gap.dart';
@@ -81,6 +82,7 @@ class _Body extends StatelessWidget {
     return Column(
       crossAxisAlignment: CrossAxisAlignment.start,
       children: [
+        const Text('kIsWasm: $kIsWasm'),
         const _TitleAndLogo(),
         Column(
           children: [
