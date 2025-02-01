73db8ec65ee8f356cb22c7f406bdbba913bfafc6
Tatsuya Okayama (blendthink) <admin@blendthink.dev>
2025-02-01 16:05:18 +0900

    refactor: Simplify SpeakerRepository by removing unused speakerStorageFileApi dependency

--- packages/common/data/lib/src/repository/speaker_repository.dart
+++ packages/common/data/lib/src/repository/speaker_repository.dart
@@ -1,22 +1,14 @@
 import 'package:common_data/src/model/speaker.dart';
-import 'package:common_data/src/supabase_client.dart';
 import 'package:riverpod/riverpod.dart';
 import 'package:riverpod_annotation/riverpod_annotation.dart';
-import 'package:supabase_flutter/supabase_flutter.dart';
 
 part 'speaker_repository.g.dart';
 
 @Riverpod(keepAlive: true)
-SpeakerRepository speakerRepository(Ref ref) => SpeakerRepository(
-      speakerStorageFileApi: ref.watch(speakerStorageFileApiProvider),
-    );
+SpeakerRepository speakerRepository(Ref ref) => SpeakerRepository();
 
 class SpeakerRepository {
-  SpeakerRepository({
-    required StorageFileApi speakerStorageFileApi,
-  }) : _speakerStorageFileApi = speakerStorageFileApi;
-
-  final StorageFileApi _speakerStorageFileApi;
+  SpeakerRepository();
 
   Speaker toSpeaker(SpeakerTable speakerTable) => Speaker(
         id: speakerTable.id,
